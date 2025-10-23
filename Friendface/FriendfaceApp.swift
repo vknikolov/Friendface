7e09d4f320f5199f82a09ae5647e7259cc024979
Veselin <Nikolov>
2025-10-23 10:07:21 +0300

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Veselin Nikolov on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendfaceTests/FriendfaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceTests.swift
+//  FriendfaceTests
+//
+//  Created by Veselin Nikolov on 23.10.25.
+//
+
+import Testing
+@testable import Friendface
+
+struct FriendfaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
